70e341ee7d83c09761ed004ccf1394d6e0ee100b
Sumbal Hashmi <115552450+sumthedev@users.noreply.github.com>
2024-05-13 15:21:03 +0500

    my-third-commit

--- src/components/header/HeaderBottom.js
+++ src/components/header/HeaderBottom.js
@@ -1,12 +1,28 @@
+import { useState, useRef, useEffect } from "react";
+import { motion } from "framer-motion";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SideNavContent from "./SideNavContent";
 
 const HeaderBottom = () => {
+  const ref = useRef();
+  const [isSideBarOpen, setSideBarOpen] = useState(false);
+  useEffect(() => {
+    document.body.addEventListener("click", (e) => {
+      if (e.target.contains(ref.current)) {
+        setSideBarOpen(false);
+      }
+    });
+  }, [ref, isSideBarOpen]);
+
   return (
     <div className="w-full px-4 h-[36px] bg-amazon_light text-white flex items-center">
       <ul className="flex items-center gap-2 text-sm tracking-wide">
-        <li className="flex items-center gap-1 headerHover">
+        <li
+          onClick={() => setSideBarOpen(true)}
+          className="flex items-center gap-1 headerHover"
+        >
           <MenuIcon />
           All
         </li>
@@ -16,6 +32,56 @@ const HeaderBottom = () => {
         <li className="hidden md:inline-flex headerHover">Registry</li>
         <li className="hidden md:inline-flex headerHover">Sell</li>
       </ul>
+      {isSideBarOpen && (
+        <div className="w-full h-screen text-black fixed top-0 left-0 bg-amazon_blue bg-opacity-50">
+          <div className="w-full h-full relative">
+            <motion.div
+              initial={{ x: -500, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              ref={ref}
+              className="w-[350px] h-full bg-white border-black"
+            >
+              <div className="w-full bg-amazon_light text-white py-2 px-6 flex items-center gap-4">
+                <AccountCircleIcon />
+                <h3 className="font-titleFont font-bold text-lg tracking-wide">
+                  Hello, Sign In
+                </h3>
+              </div>
+              <SideNavContent
+                title="Digital Content & Devices"
+                one="Amazon Music"
+                two="Kindle E-readers & Books"
+                three="Amazon Appstore"
+              />
+              <SideNavContent
+                title="Shop By Department"
+                one="Electronics"
+                two="Computers"
+                three="Smart Home"
+              />
+              <SideNavContent
+                title="Programs & Features"
+                one="Gift Cards"
+                two="Amazon live"
+                three="International Shopping"
+              />
+              <SideNavContent
+                title="Help & Settings"
+                one="Your Account"
+                two="Customer Service"
+                three="Contact us"
+              />
+            </motion.div>
+            <span
+              onClick={() => setSideBarOpen(false)}
+              className="cursor-pointer absolute top-0 left-[300px] md:left-[360px] w-10 h-10 text-black flex items-center justify-center border bg-gray-200 hover:bg-red-500 hover:text-white duration-300"
+            >
+              <CloseIcon />
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
